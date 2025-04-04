7266a4a893980816a49726d4c68af30cab8d86d4
Jainil-Patel <jainilp4321@gmail.com>
2025-04-05 00:50:04 +0530

    minor changes

--- frontend/src/components/Menubar.jsx
+++ frontend/src/components/Menubar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Menubar = () => {
   return (
-    <div className="bg-[#323232] fixed p-2 mt-16 w-full  select-none">
+    <div className="bg-[#1d1d1d] fixed p-2 mt-16 w-full  select-none">
         <div className="flex h-full justify-between items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     
             <div className="font-unbounded text-2xl font-[700] text-white ">blog</div>
