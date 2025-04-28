a1afbadf5d63fa6da7eb43decd59b5a937d3d4a4
Jainil-Patel <jainilp4321@gmail.com>
2025-04-28 20:45:31 +0530

    minor bug fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     return <nav className="bg-[#0F0F0F] fixed h-16 w-full  select-none">
         <div className="flex h-full justify-between items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     
-            <div className="font-unbounded text-3xl font-[700] text-white" onClick={() => navigate("/home")}>StuxDev</div>
+            <div className="font-unbounded text-3xl font-[700] text-white" onClick={() => navigate("/")}>StuxDev</div>
 
             <div className="h-full w-auto flex items-center justify-between gap-8 font-mono">
                 
