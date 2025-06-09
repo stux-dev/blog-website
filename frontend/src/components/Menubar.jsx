102018e6e8a083ff07d4f44ed533c2f30cbbc9d6
Jainil Patel <jainilp4321@gmail.com>
2025-06-09 14:35:26 +0530

    updated menubar

--- frontend/src/components/Menubar.jsx
+++ frontend/src/components/Menubar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const Menubar = () => {
     const navigate = useNavigate();
     return (
         <div className="bg-[#1d1d1d] fixed h-12 mt-16 w-full  select-none">
             <div className="flex h-full justify-between items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="font-unbounded text-2xl font-[700] text-white " onClick={() => navigate("/dashboard")}>
+                <div
+                    className="font-unbounded text-2xl font-[700] text-white "
+                    onClick={() => navigate("/dashboard")}
+                >
                     blog
                 </div>
 
