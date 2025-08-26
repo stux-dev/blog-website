b64922bd826f072256486f17ea044e571a212a7b
Jainil Patel <jainilp4321@gmail.com>
2025-08-26 22:43:28 +0530

    minor changes

--- frontend/src/auth/authService.jsx
+++ frontend/src/auth/authService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/auth",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
